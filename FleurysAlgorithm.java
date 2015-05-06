e551c2e826aeb6f4e72e3bbfb1e6ee3339791180
LanviN <lanvi921@gmail.com>
2015-05-05 17:58:02 -0700

    Added Edge class, changed Coordinate to Vertex

--- FleurysAlgorithm.java
+++ FleurysAlgorithm.java
@@ -4,32 +4,52 @@ public class FleurysAlgorithm{
 
 	private Random r;
 	private int size;
-	private ArrayList<Coordinate> list;
+	private ArrayList<Vertex> list;
 
 	public FleurysAlgorithm(int n){
 		this.size = n;
-		this.list = new ArrayList<Coordinate>();
+		this.list = new ArrayList<Vertex>();
 		this.r = new Random();
 		for (int x = 0; x < n; x++){
 			for (int y = 0; y < n; y++){
-				coord = new Coordinate(x, y);
+				coord = new Vertex(x, y);
 				list.add(coord)
 			}
 		}
 	}
 
 	public void generate(){
-		Coordinate start = list.get(0);
+		Vertex start = list.get(0);
 		int temp = r.nextInt(size);
 		if (list.get(temp) != start){
+			
 			//we stopped here;
 	}
 
-	private class Coordinate{
+	private class Edge{
+		private Vertex start;
+		private Vertex end;
+
+		public Edge(Vertex s, Vertex e){
+			this.start = s;
+			this.end = e;
+		}
+
+		public Vertex getStart(){
+			return this.start;
+		}
+
+		public Vertex getEnd(){
+			return this.end;
+		}
+	}
+
+	private class Vertex{
 		private int x;
 		private int y;
+		private int degree = 0;
 
-		public Coordinate(int x, int y){
+		public Vertex(int x, int y){
 			this.x = x;
 			this.y = y;
 		}
@@ -40,6 +60,14 @@ public class FleurysAlgorithm{
 
 		public int getY(){
 			return this.y;
+		}
+
+		public visit(){
+			this.degree++;
+		}
+
+		public int getDegree(){
+			return this.degree;
 		}
 	}
 }
