a352d7c4ec787a08627f0d12c70e079214158373
jony5415 <jonathannnn6@gmail.com>
2015-05-05 18:56:45 -0700

    added fxn make_lines()

--- FleurysAlgorithm.java
+++ FleurysAlgorithm.java
@@ -4,26 +4,41 @@ public class FleurysAlgorithm{
 
 	private Random r;
 	private int size;
-	private ArrayList<Vertex> list;
+	private ArrayList<Vertex> vlist;
+	private ArrayList<Edge> elist;
 
 	public FleurysAlgorithm(int n){
 		this.size = n;
-		this.list = new ArrayList<Vertex>();
+		this.vlist = new ArrayList<Vertex>();
 		this.r = new Random();
 		for (int x = 0; x < n; x++){
 			for (int y = 0; y < n; y++){
 				coord = new Vertex(x, y);
-				list.add(coord)
+				vlist.add(coord)
 			}
 		}
 	}
 
 	public void generate(){
-		Vertex start = list.get(0);
+		Vertex start = vlist.get(0);
 		int temp = r.nextInt(size);
-		if (list.get(temp) != start){
+		if (vlist.get(temp) != start){
 			
 			//we stopped here;
+	}
+
+	public static ArrayList<Edge> make_lines(ArrayList<Vertex> current) {
+
+		elist = new ArrayList<Edge>();
+
+		for (int i = 0; i < current.size(); i++ ) {
+			for (int j = i+1; j < current.size(); j++){
+				Edge e = new Edge(vlist.get(i), vlist.get(j));
+				elist.add(e);
+			}
+		}
+
+		return lines;
 	}
 
 	private class Edge{
