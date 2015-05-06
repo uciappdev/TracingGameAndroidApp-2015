f3751422ff7f9d1ef7f80ca1c1e659e8a55a4aec
Cody Han <hodycan@gmail.com>
2015-05-06 15:32:34 -0700

    Added function intersects(Edge, Edge)
    intersects still needs debugging in the return value.
    intersection coordinates are correct in the cases tested so far.
    also fixed various syntax errors and refactored some variables.

--- FleurysAlgorithm.java
+++ FleurysAlgorithm.java
@@ -1,88 +1,144 @@
+import java.util.ArrayList;
 import java.util.Random;
+
 
 public class FleurysAlgorithm{
 
-	private Random r;
-	private int size;
-	private ArrayList<Vertex> vlist;
-	private ArrayList<Edge> elist;
+    private Random randomGenerator;
+    private int size;
+    private ArrayList<Vertex> vlist;
+    private ArrayList<Edge> elist;
+    
+    // temporary main block for testing
+    public static void main(String[] args) {
+        Edge edgeOne = new Edge(new Vertex(0, 0), new Vertex(4, 4));
+        Edge edgeTwo = new Edge(new Vertex(4, 0), new Vertex(0, 4));
+        boolean intersection = intersects(edgeOne, edgeTwo);
+        System.out.println(intersection);
+    }
 
-	public FleurysAlgorithm(int n){
-		this.size = n;
-		this.vlist = new ArrayList<Vertex>();
-		this.r = new Random();
-		for (int x = 0; x < n; x++){
-			for (int y = 0; y < n; y++){
-				coord = new Vertex(x, y);
-				vlist.add(coord)
-			}
-		}
-	}
+    public FleurysAlgorithm(int n){
+        this.size = n;
+        this.vlist = new ArrayList<Vertex>();
+        this.elist = new ArrayList<Edge>();
+        this.randomGenerator = new Random();
 
-	public void generate(){
-		Vertex start = vlist.get(0);
-		int temp = r.nextInt(size);
-		if (vlist.get(temp) != start){
-			
-			//we stopped here;
-	}
+        // create all vertices. each vertex keeps count of visits.
+        for (int x = 0; x < n; x++){
+            for (int y = 0; y < n; y++){
+                Vertex coord = new Vertex(x, y);
+                vlist.add(coord);
+            }
+        }
+    }
 
-	public static ArrayList<Edge> make_lines(ArrayList<Vertex> current) {
+    // remove static from all of the following after debugging 
 
-		elist = new ArrayList<Edge>();
+    public static boolean intersects(Edge a, Edge b){
+        int ax1 = a.getStart().getX(),
+            ay1 = a.getStart().getY(),
+            ax2 = a.getEnd().getX(),
+            ay2 = a.getEnd().getY(),
+            bx1 = b.getStart().getX(),
+            by1 = b.getStart().getY(),
+            bx2 = b.getEnd().getX(),
+            by2 = b.getEnd().getY();
 
-		for (int i = 0; i < current.size(); i++ ) {
-			for (int j = i+1; j < current.size(); j++){
-				Edge e = new Edge(vlist.get(i), vlist.get(j));
-				elist.add(e);
-			}
-		}
+        
 
-		return lines;
-	}
+        int aSlope = (ay1 - ay2) / (ax1 - ax2);  // slope
+        int aLeftAxisIntersect = ay1 - aSlope*ax1;  // y = mx + b, b = mx + y
 
-	private class Edge{
-		private Vertex start;
-		private Vertex end;
+        int bSlope = (by1 - by2) / (bx1 - bx2);  
+        int bLeftAxisIntersect = by1 - bSlope*bx1;
+        System.out.println(aSlope);
+        System.out.println(aLeftAxisIntersect);
+        System.out.println(bSlope);
+        System.out.println(bLeftAxisIntersect);
 
-		public Edge(Vertex s, Vertex e){
-			this.start = s;
-			this.end = e;
-		}
+        double xIntercept = (aLeftAxisIntersect-bLeftAxisIntersect) / (bSlope-aSlope);
+        // whether you use the slopes and y axis intercept of edge a or b does not matter
+        double yIntercept = aSlope*(xIntercept) - aLeftAxisIntersect;
+        System.out.println(xIntercept);
+        System.out.println(yIntercept);
 
-		public Vertex getStart(){
-			return this.start;
-		}
 
-		public Vertex getEnd(){
-			return this.end;
-		}
-	}
+        return ((ax1 < xIntercept && xIntercept < ax2 && bx2 < xIntercept && xIntercept < bx1) &&
+                 (by2 < yIntercept && yIntercept < by1 && ay2 < yIntercept && yIntercept < ay1));
+    }
 
-	private class Vertex{
-		private int x;
-		private int y;
-		private int degree = 0;
 
-		public Vertex(int x, int y){
-			this.x = x;
-			this.y = y;
-		}
+    public void generate(){
+        for (int i=0; i < this.vlist.size(); i++){
 
-		public int getX(){
-			return this.x;
-		}
+            Vertex start = this.vlist.get(i);
+            
+            // TODO:
+            // while degree of start and end is less than 2 and the edge does not intersect
+            while (start.getDegree() < 2){
+                int temp = randomGenerator.nextInt(size);
+                Vertex end = this.vlist.get(temp);
+                if (end != start){
+                    start.visit();
+                    Edge edge = new Edge(start, end);
+                    this.elist.add(edge);
+                    break;
+                }
+            }
+        }
+    }
 
-		public int getY(){
-			return this.y;
-		}
+    public static class Edge{
+        private Vertex start;
+        private Vertex end;
 
-		public visit(){
-			this.degree++;
-		}
+        public Edge(Vertex s, Vertex e){
+            this.start = s;
+            this.end = e;
+        }
 
-		public int getDegree(){
-			return this.degree;
-		}
-	}
+        public Vertex getStart(){
+            return this.start;
+        }
+
+        public Vertex getEnd(){
+            return this.end;
+        }
+    }
+
+    public static class Vertex{
+        private int x;
+        private int y;
+        private int degree = 0;
+
+        public Vertex(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX(){
+            return this.x;
+        }
+
+        public int getY(){
+            return this.y;
+        }
+
+        public void visit(){
+            this.degree++;
+        }
+
+        public int getDegree(){
+            return this.degree;
+        }
+    }
+
+    public void makeLines(ArrayList<Vertex> current) {
+        for (int i=0; i < current.size(); i++ ) {
+            for (int j = i+1; j < current.size(); j++){
+                Edge e = new Edge(this.vlist.get(i), this.vlist.get(j));
+                this.elist.add(e);
+            }
+        }
+    }
 }
