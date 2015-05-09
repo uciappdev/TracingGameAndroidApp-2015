d859fcdf40e672066abe1ecada5caad4eb6b2936
hodycan <hodycan@gmail.com>
2015-05-08 20:14:15 -0700

    Debug intersects() and update generatePath()
    debugged and accounted for all cases of edge intersection in
    intersects()
    
    also accounted for generatePath() getting stuck by adding a limit to how
    many times it can look for the next vertex
    
    also added some whitespace

--- FleurysAlgorithm.java
+++ FleurysAlgorithm.java
@@ -2,15 +2,17 @@ import java.util.ArrayList;
 import java.util.Random;
 
 
-public class FleurysAlgorithm{
-
+public class FleurysAlgorithm {
+    private final int SEARCHLIMIT = 300;
     private Random randomGenerator;
     private int size;
     private ArrayList<Vertex> vlist;
     private ArrayList<Edge> elist;
     public ArrayList<Vertex> completeCircuit;
+    private boolean pathIsGenerated;
 
-    public FleurysAlgorithm(int size){
+
+    public FleurysAlgorithm(int size) {
         this.size = size;
         this.vlist = new ArrayList<Vertex>();
         this.elist = new ArrayList<Edge>();
@@ -18,190 +20,227 @@ public class FleurysAlgorithm{
         this.completeCircuit = new ArrayList<Vertex>();
 
         buildGrid(this.size);
-        generatePath();
+        this.pathIsGenerated = false;
+        while (!pathIsGenerated) {
+            this.pathIsGenerated = generatePath();
+        }
         System.out.println(vlist);
     }
 
-    // remove static from all of the following after debugging 
-    public void buildGrid(int size){
+
+    /*Given size, creates a grid of vertices with dimensions size x size*/
+    private void buildGrid(int size) {
         // create all vertices.
-        for (int x = 0; x < size; x++){
-            for (int y = 0; y < size; y++){
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
                 Vertex coord = new Vertex(x, y);
                 this.vlist.add(coord);
             }
         }      
     }
+    
 
-    public void generatePath(){
+    /*Generates a eulerian path in the grid.
+     * @returns: boolean representing whether or not generatePath is successful.*/
+    private boolean generatePath() {
         int r = this.randomGenerator.nextInt(this.size*this.size);
         Vertex originalStart = this.vlist.get(r);
         this.completeCircuit.add(originalStart);
         Vertex currentStart = originalStart;
-
-        while (true){
+        int searches = 0;
+        while (searches < SEARCHLIMIT) {
+            searches++;
             r = this.randomGenerator.nextInt(this.size*this.size);
             Vertex currentEnd = this.vlist.get(r);            
-            if (currentEnd.getDegree() == 2){
+            if (currentEnd.getDegree() == 2) {
                 continue; // and try a different currentEnd.
             }
-            if (currentEnd == currentStart){
+            if (currentEnd == currentStart) {
                 continue;
             }
 
             Edge potentialEdge = new Edge(currentStart, currentEnd);
-            if (intersectsAnyEdge(potentialEdge)){
+            if (intersectsAnyEdge(potentialEdge)) {
                 continue; // and try a different currentEnd
             }   
-            if (currentEnd == originalStart){
-                if (this.completeCircuit.size() >= 3){
+            if (currentEnd == originalStart) {
+                if (this.completeCircuit.size() >= 3) {
                     createEdge(currentStart, currentEnd, potentialEdge);           
-                    break;
+                    return true; // complete the circuit.
                 }
-                else if (this.completeCircuit.size() < 3){
+                else if (this.completeCircuit.size() < 3) {
                     continue;
                 }
             }            
             createEdge(currentStart, currentEnd, potentialEdge);           
             currentStart = currentEnd;
-
         }
+        return false;
     }
 
-    public void createEdge(Vertex currentStart, Vertex currentEnd, Edge potentialEdge){
-        currentStart.visit();
-        currentEnd.visit();
+    /*Actually implements a valid edge. (adds it to this.elist)*/
+    private void createEdge(Vertex start, Vertex end, Edge potentialEdge) {
+        start.visit();
+        end.visit();
         this.elist.add(potentialEdge);
-        this.completeCircuit.add(currentEnd);        
+        this.completeCircuit.add(end);        
     }
-    
-    public static boolean intersects(Edge a, Edge b){
-        int ax1 = a.getStart().getX(),
+
+    /*Given two edges, returns whether or not the two edges intersect.*/
+    public static boolean intersects(Edge a, Edge b) {
+        double ax1 = a.getStart().getX(),
             ay1 = a.getStart().getY(),
             ax2 = a.getEnd().getX(),
             ay2 = a.getEnd().getY(),
             bx1 = b.getStart().getX(),
             by1 = b.getStart().getY(),
             bx2 = b.getEnd().getX(),
-            by2 = b.getEnd().getY();
+            by2 = b.getEnd().getY(); 
 
-        int aSlope;
-        int aLeftAxisIntersect;
-        int bSlope;
-        int bLeftAxisIntersect;   
+        double aSlope;
+        double aLeftAxisIntersect;
+        double bSlope;
+        double bLeftAxisIntersect;   
            
-        int bottomSlopeFormulaA = ax1 - ax2;
-        int bottomSlopeFormulaB = bx1 - bx2;
-
+        double bottomSlopeFormulaA = ax1 - ax2;
+        double bottomSlopeFormulaB = bx1 - bx2;
 
         double xIntercept, yIntercept;
 
-        if (bottomSlopeFormulaA == bottomSlopeFormulaB){
+        if (bottomSlopeFormulaA == bottomSlopeFormulaB) {
             return false; // if both edges are vertical.
         }
-        // if edge a is vertical.
-        if (bottomSlopeFormulaA == 0){
+        // edge a is vertical.
+        if (bottomSlopeFormulaA == 0) {
             xIntercept = ax1;
             bSlope = (by1 - by2) / (bx1 - bx2);
             bLeftAxisIntersect = by1 - bSlope*bx1;  
-            yIntercept = bSlope*(xIntercept) - bLeftAxisIntersect;
-            return ((ax1 < xIntercept && xIntercept < ax2 && bx2 < xIntercept && xIntercept < bx1) &&
-                    (by1 < yIntercept && yIntercept < by2 && ay1 < yIntercept && yIntercept < ay2));  
+            yIntercept = bSlope*(xIntercept) + bLeftAxisIntersect;
+            return (isBetween(xIntercept, bx1, bx2) &&
+                    isBetween(yIntercept, by1, by2) && 
+                    isBetween(yIntercept, ay1, ay2));  
         }
-        // if edge b is vertical
-        if (bottomSlopeFormulaB == 0){
+        // edge b is vertical
+        if (bottomSlopeFormulaB == 0) {
             xIntercept = bx1;
             aSlope = (ay1 - ay2) / (ax1 - ax2);
             aLeftAxisIntersect = ay1 - aSlope*ax1;
-            yIntercept = aSlope*(xIntercept) - aLeftAxisIntersect;
-            return ((ax1 < xIntercept && xIntercept < ax2 && bx2 < xIntercept && xIntercept < bx1) &&
-                    (by1 < yIntercept && yIntercept < by2 && ay1 < yIntercept && yIntercept < ay2));  
+            yIntercept = aSlope*(xIntercept) + aLeftAxisIntersect;
+            return (isBetween(xIntercept, ax1, ax2) &&
+                    isBetween(yIntercept, by1, by2) && 
+                    isBetween(yIntercept, ay1, ay2));  
         }  
 
         aSlope = (ay1 - ay2) / (ax1 - ax2);
         bSlope = (by1 - by2) / (bx1 - bx2);
-        if (aSlope == bSlope){
+        
+        if (aSlope == bSlope) {
             return false; // parallel lines do not intersect.
         }
-        // if all the above cases are passed the only case left is:
-        //      both edges' LINES intersect and none of them are vertical.
-
-        // b = mx + y
+        // edge a is horizontal
+        if (aSlope == 0) {
+            yIntercept = ay1; // arbitrary y of horizontal edge.
+            bLeftAxisIntersect = by1 - bSlope*bx1;
+            xIntercept = (yIntercept-bLeftAxisIntersect) / bSlope;
+            return (isBetween(xIntercept, bx1, bx2) && 
+                    isBetween(xIntercept, ax1, ax2) &&
+                    isBetween(yIntercept, by1, by2));           
+        } 
+        // edge b is horizontal
+        if (bSlope == 0) {
+            yIntercept = by1; // arbitrary y of horizontal edge.
+            aLeftAxisIntersect = ay1 - aSlope*ax1;
+            xIntercept = (yIntercept-aLeftAxisIntersect) / aSlope;
+            return (isBetween(xIntercept, bx1, bx2) && 
+                    isBetween(xIntercept, ax1, ax2) &&
+                    isBetween(yIntercept, ay1, ay2));               
+        }        
+        // If all if blocks are passed then neither edges are horizontal nor vertical.
         aLeftAxisIntersect = ay1 - aSlope*ax1;
         bLeftAxisIntersect = by1 - bSlope*bx1;  
         xIntercept = (aLeftAxisIntersect-bLeftAxisIntersect) / (bSlope-aSlope);
-
-        // whether you use the slopes and y axis intercept of edge a or b does not matter
-        yIntercept = aSlope*(xIntercept) - aLeftAxisIntersect;
-
-        // return whether or not the intersection coordinates are within the boundaries of edges.
-        return ((ax1 < xIntercept && xIntercept < ax2 && bx2 < xIntercept && xIntercept < bx1) &&
-                (by1 < yIntercept && yIntercept < by2 && ay1 < yIntercept && yIntercept < ay2));            
+        // whether you use edge a or edge b is arbitrary.
+        yIntercept = aSlope*(xIntercept) + aLeftAxisIntersect;
+        return (isBetween(xIntercept, ax1, ax2) &&
+                isBetween(xIntercept, bx2, bx1) &&
+                isBetween(yIntercept, by1, by2) &&
+                isBetween(yIntercept, ay1, ay2));          
 
     }
+    
+
+    private static boolean isBetween(double x, double a, double b) {
+        return (a<x && x<b) || (b<x && x<a);
+    }
+
 
     /*checks if the given edge intersects any existing edge in the grid.*/
-    public boolean intersectsAnyEdge(Edge potentialEdge){
-        for (Edge edge : this.elist){
-            if (intersects(potentialEdge, edge)){
+    private boolean intersectsAnyEdge(Edge potentialEdge) {
+        for (Edge edge : this.elist) {
+            if (intersects(potentialEdge, edge)) {
                 return true;
             }
         }
         return false;
     }
 
-    public static class Edge{
+
+    public static class Edge {
         private Vertex start;
         private Vertex end;
 
-        public Edge(Vertex s, Vertex e){
+        public Edge(Vertex s, Vertex e) {
             this.start = s;
             this.end = e;
         }
 
-        public Vertex getStart(){
+        public Vertex getStart() {
             return this.start;
         }
 
-        public Vertex getEnd(){
+        public Vertex getEnd() {
             return this.end;
         }
     }
 
-    public static class Vertex{
+
+    public static class Vertex {
         private int x;
         private int y;
         private int degree = 0;
 
-        public Vertex(int x, int y){
+        public Vertex(int x, int y) {
             this.x = x;
             this.y = y;
         }
 
-        @Override public String toString(){
+        @Override 
+        public String toString() {
             return String.format("Vertex(%d, %d)", this.x, this.y);
         }
 
-        public int getX(){
+        public int getX() {
             return this.x;
         }
 
-        public int getY(){
+        public int getY() {
             return this.y;
         }
 
-        public void visit(){
+        public void visit() {
             this.degree++;
         }
 
-        public int getDegree(){
+        public int getDegree() {
             return this.degree;
         }
     }
 
-    public void makeLines(ArrayList<Vertex> current) {
+
+    // currently not used.
+    private void makeLines(ArrayList<Vertex> current) {
         for (int i=0; i < current.size(); i++ ) {
-            for (int j = i+1; j < current.size(); j++){
+            for (int j = i+1; j < current.size(); j++) {
                 Edge e = new Edge(this.vlist.get(i), this.vlist.get(j));
                 this.elist.add(e);
             }
