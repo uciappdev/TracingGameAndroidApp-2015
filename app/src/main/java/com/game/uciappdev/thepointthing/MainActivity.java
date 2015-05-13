c1d98da770b4dd9f44d7c4107c4d08b8c2614371
ricarg3 <ricarg3@uci.edu>
2015-05-12 18:21:24 -0700

    Game screen

--- app/src/main/java/com/game/uciappdev/thepointthing/MainActivity.java
+++ app/src/main/java/com/game/uciappdev/thepointthing/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.game_screen);
+        setContentView(R.layout.activity_main);
     }
 
     @Override
